c9b4ecb265e147912e70319c079f06d45af36779
MSan35 <matteo_sansone2003@yahoo.ca>
2025-03-28 12:08:39 -0400

    setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import HomePage from './pages/homePage';
+
+function App() {
+  return (
+    <div className="bg-black text-white min-h-screen p-4 flex flex-col items-center">
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;

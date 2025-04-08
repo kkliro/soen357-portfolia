8c8348ee922dc91a18a51c8651b9aca689f516f8
MSan35 <148164426+MSan35@users.noreply.github.com>
2025-04-08 18:15:36 -0400

    added some pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import HomePage from './pages/homePage'
 import LandingPage from './pages/landingPage'
+import LoginPage from './pages/loginPage'
+import CreateAccountPage from './pages/createAccountPage'
 
 function App() {
   return (
     <>
-      <LandingPage />
+      <CreateAccountPage />
     </>
   )
 }

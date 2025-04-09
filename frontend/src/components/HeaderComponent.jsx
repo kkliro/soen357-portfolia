63d943dffebc0ad9c2f9606ba3e99367793bafd1
MSan35 <148164426+MSan35@users.noreply.github.com>
2025-04-09 00:19:43 -0400

    added user icon

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import logo from '../assets/logo.png'
+import user_icon from '../assets/user_icon.png'
 import { AuthContext } from '../context/AuthContext'
 import { logoutUser } from '../hooks/auth/logout'
 
@@ -27,25 +28,25 @@ export default function HeaderComponent() {
 
   return (
     <nav className="fixed top-0 w-full bg-black bg-opacity-70 backdrop-blur-lg z-50 border-b border-purple-900">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           
           {/* Logo + Title */}
           <div className="flex items-center gap-2">
-            <img alt="Portfolia Logo" src={logo} className="h-10 w-10" />
-            <span className="text-2xl font-bold text-purple-400">Portfolia</span>
+            <img alt="Portfolia Logo" src={logo} className="h-12 w-12" />
+            <span className="text-3xl font-bold text-purple-400">Portfolia</span>
           </div>
 
           {/* Navigation */}
-          <div className="hidden sm:flex items-center space-x-4">
+          <div className="flex items-center space-x-4 ml-auto">
             {navigation.map((item) => (
               <a
                 key={item.name}
                 href={item.href}
                 className={`${
                   item.current
-                    ? 'bg-indigo-800 text-white'
-                    : 'text-white hover:bg-indigo-600 hover:text-white'
+                    ? 'bg-indigo-800 text-purple-400'
+                    : 'text-purple-400 hover:bg-indigo-600 hover:text-white'
                 } rounded-md px-4 py-2 text-sm font-medium transition-colors duration-200`}
               >
                 {item.name}
@@ -53,7 +54,7 @@ export default function HeaderComponent() {
             ))}
             <button
               onClick={handleLogout}
-              className=" hover:bg-indigo-600 text-white rounded-md px-4 py-2 text-sm font-medium transition-colors duration-200"
+              className=" hover:bg-indigo-600 hover:text-white text-purple-400 rounded-md px-4 py-2 text-sm font-medium transition-colors duration-200"
             >
               Logout
             </button>

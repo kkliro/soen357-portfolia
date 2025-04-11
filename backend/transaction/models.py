1a512d4f20dd145f137f7e7010fe981e4c1d5bce
kkliro <konstantin-klir@outlook.com>
2025-04-11 00:41:37 -0400

    transaction: updated the date field to be modifiable for testing

--- backend/transaction/models.py
+++ backend/transaction/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from portfolio.models import Portfolio
 
 class Transaction(models.Model):
@@ -15,7 +16,7 @@ class Transaction(models.Model):
     quantity = models.DecimalField(max_digits=20, decimal_places=8, help_text="Number of units involved in the transaction.")
     price_per_unit = models.DecimalField(max_digits=20, decimal_places=8, help_text="Price per unit at the time of transaction.")
     total_cost = models.DecimalField(max_digits=20, decimal_places=8, help_text="Total cost of the transaction.")
-    transaction_date = models.DateTimeField(auto_now_add=True)
+    transaction_date = models.DateTimeField(default=timezone.now)
     
     def save(self, *args, **kwargs):
         self.total_cost = self.quantity * self.price_per_unit
